fb5355f9fed0a0b27f194c7ddf664dea55e7705b
Carl Mungazi <carl.mungazi@airfinetworks.com>
2018-11-01 18:46:47 +0000

    Create more components

---
+++ src/docs/examples/Label/ExampleLabel.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Label from 'react-input/Label';
+
+export default function ExampleLabel() {
+  return <Label htmlFor="test" label="test" required />
+}
---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'react-input/ProgressBar';
+
+/** Show 10% progress */
+export default function Example10Percent() {
+  return <ProgressBar percent={10} width={150} />
+}
