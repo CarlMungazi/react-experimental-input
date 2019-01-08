b6c498bd97c7285202472ea48d29cdd8570e2f42
Carl Mungazi <carl.mungazi@airfinetworks.com>
2019-01-08 18:33:33 +0000

    Updates

---
+++ precache-manifest.e6dd4e94152a3dd578d7802abcb097a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61ba6c4420468fae5340",
+    "url": "/static/js/main.61ba6c44.chunk.js"
+  },
+  {
+    "revision": "0956de1313301a1f48db",
+    "url": "/static/js/1.0956de13.chunk.js"
+  },
+  {
+    "revision": "61ba6c4420468fae5340",
+    "url": "/static/css/main.6b482653.chunk.css"
+  },
+  {
+    "revision": "0956de1313301a1f48db",
+    "url": "/static/css/1.f5bce03b.chunk.css"
+  },
+  {
+    "revision": "f75015cc09bcdedfb5da7261f1390e1c",
+    "url": "/index.html"
+  }
+];
