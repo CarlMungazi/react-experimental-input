a0005be1aea9fb101161aa5e0a8088dff4ff006f
Carl Mungazi <carl.mungazi@airfinetworks.com>
2019-03-05 13:07:57 +0000

    Edit configuration stuff

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Icon} from './Icon';
+export {default as ProgressBar} from './ProgressBar';
+export {default as Label} from './Label';
+export {default as Input} from './Input';
+export {default as PasswordInput} from './PasswordInput';
